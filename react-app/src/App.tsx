79680e8a4c83a3b9195e0e5abce262438f9a9539
João Vitor <joaozanini.code@gmail.com>
2023-05-29 17:48:06 -0300

    .

---
+++ react-app/src/App.tsx
@@ -0,0 +1,7 @@
+import message from "./Message";
+
+function App() {
+  return <div><message></message></div>
+}
+
+export default App;
---
+++ react-app/src/Message.tsx
@@ -0,0 +1,5 @@
+function message() {
+    return <h1>Hello, World!</h1>
+}
+
+export default message;
